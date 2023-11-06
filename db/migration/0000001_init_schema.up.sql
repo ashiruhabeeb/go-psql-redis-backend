a820ecb66010299880519e7d12b1fe8bc9cb82c9
DeWalle <ashiruhabeeb47@gmail.com>
2023-11-06 20:03:16 +0100

    database migration files defined

---
+++ db/migration/0000001_init_schema.up.sql
@@ -0,0 +1,31 @@
+-- PSQL Extension: install uuid extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Table: user
+CREATE TABLE IF NOT EXISTS "users" (
+    userid      uuid    DEFAULT uuid_generate_v4 (),
+    firstname   VARCHAR(225) NOT NULL,
+    lastname    VARCHAR(225) NOT NULL,
+    username    VARCHAR(225) NOT NULL UNIQUE,
+    email       VARCHAR(225) NOT NULL UNIQUE,
+    password    VARCHAR(225) NOT NULL,
+    phone       VARCHAR(20) NOT NULL UNIQUE,
+    address_id  uuid NUT NULL,
+    craeted_on  TIMESTAMPTZ NOT NULL DEFAULT (now()),
+    updated_on  TIMESTAMPTZ,
+    PRIMARY KEY(userid)
+);
+
+-- Table: user address
+CREATE TABLE IF NOT EXISTS "address" (
+    address_id    uuid    DEFAULT uuid_genearte_v4 (),
+    housenumber VARCHAR(225) NOT NULL,
+    streetname  VARCHAR(225) NOT NULL,
+    localarea   VARCHAR(225) NOT NULL,
+    state       VARCHAR(225) NOT NULL,
+    country     VARCHAR(225) NOT NULL,
+    PRIMARY KEY(address_id)
+);
+
+-- Table data definition commands
+ALTER TABLE "users" ADD FOREIGN KEY ("address_id") REFERENCES "address" ("addres_id");
